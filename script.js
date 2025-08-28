e63796fac32fe578e443660c2565c9bf8176518f
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-29 02:37:46 +0600

    heart icons functionality completed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('connected');
+
+// heart increase functionality 
+
+let heartCount = parseInt(document.getElementById('heart-count').innerText);
+
+const heartIcons =document.getElementsByClassName("fa-heart");
+for(const heart of heartIcons){
+ 
+ heart.addEventListener('click',function(e)
+{
+
+    heartCount+=1;
+    document.getElementById('heart-count').innerText=heartCount;
+    
+})
+ 
+}
+
+
+
+
+
