ec2f1a19ec96123069ad8749effee8452895e2be
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-29 03:47:38 +0600

    calling feature added

--- script.js
+++ script.js
@@ -17,6 +17,39 @@ for(const heart of heartIcons){
  
 }
 
+// call functionality 
+
+
+const callBtns = document.getElementsByClassName('call-btn');
+
+
+for(const callBtn of callBtns){
+   callBtn.addEventListener('click',function(e){
+    const serviceName =e.target.parentNode.parentNode.getElementsByClassName('service-name')[0].innerText;
+    
+    const serviceNumber=e.target.parentNode.parentNode.getElementsByClassName('service-number')[0].innerText;
+    
+    
+    let availableCoin =parseInt(document.getElementById('coin-count').innerText);
+    if(availableCoin>=20)
+    {
+        alert('Calling '+ serviceName +" : "+ serviceNumber);
+        availableCoin-=20;
+        document.getElementById('coin-count').innerText=availableCoin;
+    }
+    else{
+        alert("You do not have sufficient Balance. You need minimum 20 coins to make a call")
+    }
+
+        
+        
+   })
+    
+}
+
+
+
+
 
 
 
