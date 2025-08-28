2577176904eac3411519c8caefb1e5ea5e3d0546
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-29 04:23:16 +0600

    call history functionality done

--- script.js
+++ script.js
@@ -22,12 +22,13 @@ for(const heart of heartIcons){
 
 const callBtns = document.getElementsByClassName('call-btn');
 
+const callHistory =[];
 
 for(const callBtn of callBtns){
    callBtn.addEventListener('click',function(e){
-    const serviceName =e.target.parentNode.parentNode.getElementsByClassName('service-name')[0].innerText;
+    const serviceName =e.currentTarget.parentNode.parentNode.getElementsByClassName('service-name')[0].innerText;
     
-    const serviceNumber=e.target.parentNode.parentNode.getElementsByClassName('service-number')[0].innerText;
+    const serviceNumber=e.currentTarget.parentNode.parentNode.getElementsByClassName('service-number')[0].innerText;
     
     
     let availableCoin =parseInt(document.getElementById('coin-count').innerText);
@@ -36,6 +37,33 @@ for(const callBtn of callBtns){
         alert('Calling '+ serviceName +" : "+ serviceNumber);
         availableCoin-=20;
         document.getElementById('coin-count').innerText=availableCoin;
+
+        const history = {
+            historyTitle:serviceName,
+            historyNumber: serviceNumber,
+            date: new Date().toLocaleTimeString()
+        }
+
+        callHistory.push(history);
+        const historyList =document.getElementById("history-list");
+        historyList.innerText='';
+    for( const item of callHistory){
+    const div = document.createElement('div');
+    div.innerHTML=`              <div
+                class="flex justify-between items-center mx-[24px] mt-[24px] p-[16px] bg-[#FAFAFA] rounded-[8px]"
+              >
+                <div>
+                  <h1 class="font-semibold text-[18px]">${item.historyTitle}</h1>
+                  <p class="text-gray-500">${item.historyNumber}</p>
+                </div>
+                <div>
+                  <p class="text-[18px] font-normal">${item.date}</p>
+                </div>
+              </div>`
+              historyList.appendChild(div);
+
+}
+        
     }
     else{
         alert("You do not have sufficient Balance. You need minimum 20 coins to make a call")
@@ -46,6 +74,10 @@ for(const callBtn of callBtns){
    })
     
 }
+
+// showing history 
+
+
 
 
 
